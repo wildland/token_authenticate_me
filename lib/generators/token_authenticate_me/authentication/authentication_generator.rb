d6f266eec2d73942ea55c9663addfbb21ae2da1a
Sam Clopton <samsinite@gmail.com>
2014-11-13 21:08:59 -0800

    WIP...

--- lib/generators/token_authenticate_me/authentication/authentication_generator.rb
+++ lib/generators/token_authenticate_me/authentication/authentication_generator.rb
@@ -2,9 +2,14 @@ module TokenAuthenticateMe
   module Generators
     class AuthenticationGenerator < ::Rails::Generators::NamedBase
       include Rails::Generators::Migration
-      
+
       source_root File.expand_path('../templates', __FILE__)
       check_class_collision suffix: ''
+
+      def self.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        ActiveRecord::Migration.next_migration_number(next_migration_number)
+      end
 
       def create_authentication_model_file
         template 'model.rb', File.join('app/models', "#{singular_name}.rb")
