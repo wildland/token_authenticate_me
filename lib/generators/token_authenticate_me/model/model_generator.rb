abb24843a83d7a9b2f0e3de91533fce657ecffab
Sam Clopton <samsinite@gmail.com>
2014-11-13 20:40:09 -0800

    WIP...

---
+++ lib/generators/token_authenticate_me/model/model_generator.rb
@@ -0,0 +1,9 @@
+module TokenAuthenticateMe
+  module Generators
+    class AuthenticationGenerator < ::Rails::Generators::NamedBase
+      source_root File.expand_path('../templates', __FILE__)
+      check_class_collision suffix: ''
+
+      def create_authentication_model_file
+        template 'model.rb', File.join('app/models', "#{singular_name}.rb")
+      end
--- lib/generators/token_authenticate_me/model/templates/model.rb
+++ lib/generators/token_authenticate_me/model/templates/model.rb
@@ -1,6 +1,6 @@
-require 'token_authenticate_me'
+require 'token_authenticate_me/authentication_model'
 
 class <%= class_name %> < ActiveRecord::Base
   include TokenAuthenticateMe::AuthenticationModel
-  
+
 end
