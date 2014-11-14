61953b751a095bfc2c854f76a8bf1c49b828ee0d
Sam Clopton <samsinite@gmail.com>
2014-11-13 20:33:18 -0800

    WIP...

--- lib/token_authenticate_me.rb
+++ lib/token_authenticate_me.rb
@@ -1,4 +1,5 @@
 require 'token_authenticate_me/authentication_model'
+require 'token_authenticate_me/engine'
 require 'token_authenticate_me/version'
 
 module TokenAuthenticateMe
---
+++ lib/token_authenticate_me/engine.rb
@@ -0,0 +1,5 @@
+module TokenAuthenticateMe
+  class Engine < Rails::Engine
+    engine_name :token_authenticate_me
+  end
+end
