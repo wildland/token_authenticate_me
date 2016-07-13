40befc394b9449e3a7ee15533d576116bbbf27f8
Joe Weakley <joew@samjoe.com>
2016-07-13 15:27:57 -0700

    Fixes #19.

---
+++ lib/token_authenticate_me/controllers/token_authenticateable.rb
@@ -0,0 +1,53 @@
+require 'active_support/concern'
+
+module TokenAuthenticateMe
+  module Controllers
+    module TokenAuthenticateable
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :authenticate
+      end
+
+      protected
+
+      def authenticate
+        authenticate_token || authenticate_param || render_unauthorized
+      end
+
+      def current_user
+        if authenticate_token
+          @current_user ||= User.find_by_id(authenticate_token.user_id)
+        else
+          nil
+        end
+      end
+
+      def authenticate_token
+        @session ||= authenticate_with_http_token do |token, _options|
+          session = Session.find_by_key(token)
+          if session && session.expiration > DateTime.now
+            session
+          else
+            false
+          end
+        end
+      end
+
+      def authenticate_param
+        token = params[:authentication_token]
+        session = Session.find_by_key(token)
+        if session && session.expiration > DateTime.now
+          @session ||= session
+        else
+          @session ||= false
+        end
+      end
+
+      def render_unauthorized
+        headers['WWW-Authenticate'] = 'Token realm="Application"'
+        render json: 'Bad credentials', status: 401
+      end
+    end
+  end
+end
